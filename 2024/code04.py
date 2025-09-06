e040a5d20674dff780fcf197cb79e167759699b7
Dave Schaaf <davidschaaf@berkeley.edu>
2025-09-06 18:20:12 -0500

    Finds correct number of XM

--- 2024/code04.py
+++ 2024/code04.py
@@ -13,6 +13,24 @@ def find_m(data: str):
     return -1
 
 def find_xmas(data: str):
-    return find_x(data) >= 0 and find_m(data)>= 0  
+    row_n: int = len(data)
+    col_n: int = len(data[0])
+    m_stack: [int] = []
+    xmas: int = 0
+    for row in range(row_n):
+        for col in range(col_n):
+            if data[row][col] == "X":
+                print(f"Found X at: {row},{col}")
+                for r in range(row-1,row+2):
+                    if r < 0 or r > row_n -1:
+                        continue
+                    for c in range(col-1,col+2):
+                        if c < 0 or c > col_n -1:
+                            continue
+                        print(data[r][c])
+                        if data[r][c] == "M":
+                            print(f"Found M at :{r},{c}")
+                            xmas += 1
+    return xmas
 
 
