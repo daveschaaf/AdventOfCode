0addd06734fdd922d1f66472c6fad6a77fadb026
Dave Schaaf <davidschaaf@berkeley.edu>
2025-10-05 18:38:16 -0400

    feat: set and get functions for the map

--- 2024/Day8/day08.py
+++ 2024/Day8/day08.py
@@ -16,6 +16,8 @@ sample_1 = """..........
 class AntennaMap():
     def __init__(self, map_blob):
         self.map = self.parse_map(map_blob)
+        self.rows_range = range(len(self.map))
+        self.cols_range = range(len(self.map[0]))
         self.antennas = self.find_antennas()
         
     def parse_map(self, map_blob):
@@ -29,6 +31,24 @@ class AntennaMap():
                 if self.map[row][col] != '.':
                     antennas.append((row,col))
         return antennas
+
+    def set_antinodes(self, antenna_1, antenna_2):
+        pass
+
+    def set(self, value, row, col):
+        if self._accessible(row, col):
+            self.map[row][col] = value
+            return value
+        return False
+
+    def get(self, row, col):
+        if self._accessible(row, col):
+            return self.map[row][col]
+        return False
+
+    def _accessible(self, row, col):
+        return row in self.rows_range and col in self.cols_range
+
 
     
     
--- 2024/Day8/test_day08.py
+++ 2024/Day8/test_day08.py
@@ -25,9 +25,22 @@ def test_parse_sample():
     assert map_1[5][5] == "a"
     assert map_1[7][6] == "#"
 
+
 def test_find_antennas():
     map_1 = map_sample_1()
+    antennas = map_1.find_antennas()
+    assert (3,4) in map_1.find_antennas()
     assert (3,4) in map_1.antennas
+    assert (5,5) in map_1.find_antennas()
     assert (5,5) in map_1.antennas
 
+def test_set_get():
+    map_1 = map_sample_1()
+
+    assert map_1.set("@", 1,2) == "@"
+    assert map_1.map[1][2] == "@"
+    assert map_1.get(1,2) == "@"
+    
+    assert map_1.set("@", 100,2) == False
+    assert map_1.get(100,2) == False
     
