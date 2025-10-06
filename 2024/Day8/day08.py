ad67c2128ada43ab70885a1a33aa61117d44a20d
Dave Schaaf <davidschaaf@berkeley.edu>
2025-10-05 21:42:45 -0400

    wip: part 2 failing too low ;

--- 2024/Day8/day08.py
+++ 2024/Day8/day08.py
@@ -11,13 +11,22 @@ class AntennaMap():
         self.antennas = self.find_antennas()
         self.antinodes = defaultdict(set)
         for antenna, locations in self.antennas.items():
-            self.set_antinodes(antenna, locations)
+            self.set_antinodes_part_1(antenna, locations)
+        self.antinodes_part_2 = defaultdict(set)
+        for antenna, locations in self.antennas.items():
+            self.set_antinodes_part_2(antenna, locations)
 
 
     def part_1(self):
         compiled_antinodes = set()
         for antinodes in self.antinodes.values():
-            print(antinodes)
+            for node in antinodes:
+                compiled_antinodes.add(node)
+        return len(compiled_antinodes)
+    
+    def part_2(self):
+        compiled_antinodes = set()
+        for antinodes in self.antinodes_part_2.values():
             for node in antinodes:
                 compiled_antinodes.add(node)
         return len(compiled_antinodes)
@@ -36,6 +45,9 @@ class AntennaMap():
         return antennas
 
     def set_antinodes(self,value, antennas, mark = ANTINODE):
+        self.set_antinodes_part_1(value, antennas, mark)
+
+    def set_antinodes_part_1(self,value, antennas, mark = ANTINODE):
         for i in range(len(antennas) - 1):
             for j in range(i+1, len(antennas)):
                 ant1 = antennas[i]
@@ -53,6 +65,32 @@ class AntennaMap():
 
                 self.add_antinode(value, ant2[0] + vector2[0], ant2[1] + vector2[1])
                 self.set(mark, ant2[0] + vector2[0], ant2[1] + vector2[1])
+
+    def set_antinodes_part_2(self,value, antennas, mark = ANTINODE):
+        for i in range(len(antennas) - 1):
+            for j in range(i+1, len(antennas)):
+                ant1 = antennas[i]
+                ant2 = antennas[j]
+                vector1 = (
+                    ant1[0] - ant2[0],
+                    ant1[1] - ant2[1]
+                )
+                vector2 = (
+                    vector1[0] * -1,
+                    vector1[1] * -1
+                )
+
+                node_vector1 = (ant1[0] + vector1[0], ant1[1] + vector1[1])
+                while self._accessible(*node_vector1):
+                    self.antinodes_part_2[value].add(node_vector1)
+                    node_vector1 = (node_vector1[0] + vector1[0], node_vector1[1] + vector1[1])
+
+
+                node_vector2 = (ant2[0] + vector2[0], ant2[1] + vector2[1])
+                while self._accessible(*node_vector2):
+                    self.antinodes_part_2[value].add(node_vector2)
+                    node_vector2 = (node_vector2[0] + vector2[0], node_vector2[1] + vector2[1])
+
     
     def add_antinode(self, antenna, row, col):
         if self._accessible(row, col):
