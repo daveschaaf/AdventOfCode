8e4b21b00b59e00a300ae287077f222e0085d4f7
Dave Schaaf <davidschaaf@berkeley.edu>
2025-10-05 20:14:44 -0400

    feat: set antinodes for multiple antennae

--- 2024/Day8/day08.py
+++ 2024/Day8/day08.py
@@ -1,19 +1,9 @@
 # 2024 Day 8 Code
 
-sample_1 = """..........
-...#......
-..........
-....a.....
-..........
-.....a....
-..........
-......#...
-..........
-.........."""
-
-
 
 class AntennaMap():
+
+    ANTINODE = "%"
     def __init__(self, map_blob):
         self.map = self.parse_map(map_blob)
         self.rows_range = range(len(self.map))
@@ -32,10 +22,25 @@ class AntennaMap():
                     antennas.append((row,col))
         return antennas
 
-    def set_antinodes(self, antenna_1, antenna_2):
-        pass
+    def set_antinodes(self, antennas, mark = ANTINODE):
+        for i in range(len(antennas) - 1):
+            for j in range(i+1, len(antennas)):
+                ant1 = antennas[i]
+                ant2 = antennas[j]
+                print(f"{ant1=}, {ant2=}")
+                vector1 = (
+                    ant1[0] - ant2[0],
+                    ant1[1] - ant2[1]
+                )
+                vector2 = (
+                    vector1[0] * -1,
+                    vector1[1] * -1
+                )
+                self.set(mark, ant1[0] + vector1[0], ant1[1] + vector1[1])
+                self.set(mark, ant2[0] + vector2[0], ant2[1] + vector2[1])
 
     def set(self, value, row, col):
+        #print(f"set: {value=} {row=} {col=}")
         if self._accessible(row, col):
             self.map[row][col] = value
             return value
--- 2024/Day8/test_day08.py
+++ 2024/Day8/test_day08.py
@@ -13,34 +13,68 @@ sample_1 = """..........
 ..........
 .........."""
 
-def map_sample_1(sample = sample_1):
+sample_2 = """..........
+...#......
+#.........
+....a.....
+........a.
+.....a....
+..#.......
+......#...
+..........
+.........."""
+
+
+def map_sample(sample = sample_1):
     return AntennaMap(sample)
 
-def test_parse_sample():
-    map_1 = map_sample_1().map
-    assert len(map_1) == 10
-    assert len(map_1[0]) == 10
-    assert map_1[1][3] == "#"
-    assert map_1[3][4] == "a"
-    assert map_1[5][5] == "a"
-    assert map_1[7][6] == "#"
+
+def test_parse_map():
+    map_1 = map_sample().map
+    assert len(map_1) == 10, 'has the correct number of rows'
+    assert len(map_1[0]) == 10, 'has the correct number of columns'
+    assert map_1[1][3] == "#", 'sets data in the correct position'
+    assert map_1[3][4] == "a", 'sets data in the correct position'
+
+    assert map_1[5][5] == "a", 'sets data in the correct position'
+
+    assert map_1[7][6] == "#", 'sets data in the correct position'
+
 
 
 def test_find_antennas():
-    map_1 = map_sample_1()
+    map_1 = map_sample()
     antennas = map_1.find_antennas()
-    assert (3,4) in map_1.find_antennas()
-    assert (3,4) in map_1.antennas
-    assert (5,5) in map_1.find_antennas()
-    assert (5,5) in map_1.antennas
+    assert (3,4) in map_1.find_antennas(), "finds antennas on the map"
+    assert (3,4) in map_1.antennas, "finds antennas on the map"
+    assert (5,5) in map_1.find_antennas(), "finds antennas on the map"
+    assert (5,5) in map_1.antennas, "finds antennas on the map"
 
 def test_set_get():
-    map_1 = map_sample_1()
+    map_1 = map_sample()
 
-    assert map_1.set("@", 1,2) == "@"
-    assert map_1.map[1][2] == "@"
-    assert map_1.get(1,2) == "@"
+    assert map_1.set("@", 1,2) == "@", 'sets a value within bounds'
+    assert map_1.map[1][2] == "@", 'sets a value within bounds'
+    assert map_1.get(1,2) == "@", 'accesses a value within bounds'
     
-    assert map_1.set("@", 100,2) == False
-    assert map_1.get(100,2) == False
-    
+    assert map_1.set("@", 100,2) == False, 'does not set a value outside the rows'
+    assert map_1.set("@", 2,200) == False, 'does not set a value outside the columns'
+    assert map_1.get(100,2) == False, 'does not access a value outside the rows'
+    assert map_1.get(2,200) == False, 'does not access a value outside the columns'
+
+def test_set_antinodes():
+    map_1 = map_sample()
+    map_1.set_antinodes([(3,4),(5,5)],mark = "%")
+    assert map_1.get(1,3) == "%"
+    assert map_1.get(7,6) == "%"
+
+    map_2 = map_sample(sample_2)
+    assert map_2.get(3,4) == 'a'
+    assert map_2.get(5,5) == 'a'
+    assert map_2.get(4,8) == 'a'
+    map_2.set_antinodes([(3,4),(5,5),(4,8)],mark = "%")
+    assert map_2.get(1,3) == "%"
+    assert map_2.get(7,6) == "%"
+    assert map_2.get(2,0) == "%"
+    assert map_2.get(6,2) == "%"
+
