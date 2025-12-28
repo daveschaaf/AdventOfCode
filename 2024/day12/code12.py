e80076b7246b76de61120be61ca51804fc6a03ba
David Schaaf <davidschaaf@berkeley.edu>
2025-12-28 12:28:10 -0500

    Part 2 complete

--- 2024/day12/code12.py
+++ 2024/day12/code12.py
@@ -10,34 +10,107 @@ class Region():
         self.garden = parsed_map
         self.nrow = len(parsed_map)
         self.ncol = len(parsed_map[0])
-        self.visited = set()
+        self.plots = set()
+        self.sides = 0
         self.plot_garden(starting_cell)
         
     def price(self):
         return self.area * self.perimeter
+    def discounted_price(self):
+        return self.area * self.sides
 
     def plot_garden(self, cell):
         value = self.get_value(*cell)
-        self.perimeter = self.dfs_perimeter(cell, value)
+        self.perimeter, _ = self.dfs_perimeter(cell, value)
 
     def dfs_perimeter(self, cell, value):
         if self.get_value(*cell) != value:
-            return 1
-        if cell in self.visited:
-            return 0
-        self.visited.add(cell)
+            return 1, True
+        if cell in self.plots:
+            return 0, False
+        self.plots.add(cell)
         self.area += 1
         
-        up = (cell[0]-1, cell[1])
-        down = (cell[0]+1, cell[1])
-        left = (cell[0], cell[1]-1)
-        right = (cell[0], cell[1]+1)
-        directions = [up, down, left, right]
-        return sum(map(lambda d: self.dfs_perimeter(d, value), directions))
+        local_boundaries = {}
+        total_edges = 0
+        for direction, neighbor_coords in self.ortho_neighbors(cell).items():
+            edge_count, is_boundary = self.dfs_perimeter(neighbor_coords, value)
+            total_edges += edge_count
+            local_boundaries[direction] = [neighbor_coords, is_boundary]
+        self.sides += self.plot_corners(local_boundaries, cell, value) 
+        return total_edges, False 
+    
+    def plot_corners(self, local_boundaries, cell, value):
+        def outside_region(neighbor):
+            return self.get_value(*neighbor) != value
+        corners = 0
 
+        up = local_boundaries['up'][1]
+        down = local_boundaries['down'][1]
+        left = local_boundaries['left'][1]
+        right = local_boundaries['right'][1]
+        corners += sum([up and right, up and left, down and right, down and left])
         
+        local_diagonals = self.diag_neighbors(cell)
+        ul = not up and not left and outside_region(local_diagonals['ul'])
+        ur = not up and not right and outside_region(local_diagonals['ur'])
+        dl = not down and not left and outside_region(local_diagonals['dl'])
+        dr = not down and not right and outside_region(local_diagonals['dr'])
+        corners += sum([ul, ur, dl, dr])
+        return corners
+
     def get_value(self, row, col):
         if row < 0 or col < 0 or row > self.nrow-1 or col > self.ncol-1:
             return None
         return self.garden[row][col]
+    
+    def diag_neighbors(self, cell):
+        diag_keys = ['ul', 'ur', 'dr', 'dl']
+        all_directions = self.neighbors(cell)
+        return {key: all_directions[key] for key in diag_keys}
 
+    def ortho_neighbors(self, cell):
+        ortho_keys = ['up', 'down', 'left', 'right']
+        all_directions = self.neighbors(cell)
+        return {key: all_directions[key] for key in ortho_keys}
+    
+    def neighbors(self, cell):
+        up = (cell[0]-1, cell[1])
+        down = (cell[0]+1, cell[1])
+        left = (cell[0], cell[1]-1)
+        right = (cell[0], cell[1]+1)
+        ul = (cell[0]-1, cell[1]-1)
+        ur = (cell[0]-1, cell[1]+1)
+        dl = (cell[0]+1, cell[1]-1)
+        dr = (cell[0]+1, cell[1]+1)
+        udlr = {"up": up, "down": down, "left": left,"right": right,
+                'ul': ul, 'ur': ur, 'dl': dl, 'dr': dr}
+        return udlr 
+
+
+def part1(raw_map):
+    parsed_map = parse_map(raw_map)
+    total_price = 0
+    plotted = set()
+    for row in range(len(parsed_map)):
+        for col in range(len(parsed_map[0])):
+            plot = (row, col)
+            if plot not in plotted:
+                region = Region(parsed_map, plot)
+                total_price += region.price()
+                plotted = plotted.union(region.plots)
+    return total_price
+
+
+def part2(raw_map):
+    parsed_map = parse_map(raw_map)
+    total_price = 0
+    plotted = set()
+    for row in range(len(parsed_map)):
+        for col in range(len(parsed_map[0])):
+            plot = (row, col)
+            if plot not in plotted:
+                region = Region(parsed_map, plot)
+                total_price += region.discounted_price()
+                plotted = plotted.union(region.plots)
+    return total_price
