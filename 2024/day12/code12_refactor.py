39e0d2e4650173879d82f8a7ab802c19979e61be
David Schaaf <davidschaaf@berkeley.edu>
2026-01-16 14:16:10 -0500

    plots refactored to use a stack instead of recursion

---
+++ 2024/day12/code12_refactor.py
@@ -0,0 +1,134 @@
+
+def parse_map(raw_map):
+    rows = raw_map.split('\n')
+    return [list(row) for row in rows]
+
+class Region():
+    def __init__(self, parsed_map, starting_cell):
+        self.area = 0
+        self.perimeter = 0
+        self.garden = parsed_map
+        self.nrow = len(parsed_map)
+        self.ncol = len(parsed_map[0])
+        self.plots = set()
+        self.sides = 0
+        self.plot_garden(starting_cell)
+        
+    def price(self):
+        return self.area * self.perimeter
+    def discounted_price(self):
+        return self.area * self.sides
+
+    def plot_garden(self, cell):
+        value = self.get_value(*cell)
+        self.perimeter, _ = self.dfs_perimeter(cell, value)
+
+    def dfs_perimeter(self, cell, value):
+        if self.get_value(*cell) != value:
+            return 1, True
+        if cell in self.plots:
+            return 0, False
+        self.plots.add(cell)
+        self.area += 1
+        
+        local_boundaries = {}
+        total_edges = 0
+        for direction, neighbor_coords in self.ortho_neighbors(cell).items():
+            edge_count, is_boundary = self.dfs_perimeter(neighbor_coords, value)
+            total_edges += edge_count
+            local_boundaries[direction] = [neighbor_coords, is_boundary]
+        self.sides += self.plot_corners(local_boundaries, cell, value) 
+        return total_edges, False 
+    
+    def plot_corners(self, local_boundaries, cell, value):
+        def outside_region(neighbor):
+            return self.get_value(*neighbor) != value
+        corners = 0
+
+        up = local_boundaries['up'][1]
+        down = local_boundaries['down'][1]
+        left = local_boundaries['left'][1]
+        right = local_boundaries['right'][1]
+        corners += sum([up and right, up and left, down and right, down and left])
+        
+        local_diagonals = self.diag_neighbors(cell)
+        ul = not up and not left and outside_region(local_diagonals['ul'])
+        ur = not up and not right and outside_region(local_diagonals['ur'])
+        dl = not down and not left and outside_region(local_diagonals['dl'])
+        dr = not down and not right and outside_region(local_diagonals['dr'])
+        corners += sum([ul, ur, dl, dr])
+        return corners
+
+    def get_value(self, row, col):
+        if row < 0 or col < 0 or row > self.nrow-1 or col > self.ncol-1:
+            return None
+        return self.garden[row][col]
+    
+    def diag_neighbors(self, cell):
+        diag_keys = ['ul', 'ur', 'dr', 'dl']
+        all_directions = self.neighbors(cell)
+        return {key: all_directions[key] for key in diag_keys}
+
+    def ortho_neighbors(self, cell):
+        ortho_keys = ['up', 'down', 'left', 'right']
+        all_directions = self.neighbors(cell)
+        return {key: all_directions[key] for key in ortho_keys}
+    
+    def neighbors(self, cell):
+        up = (cell[0]-1, cell[1])
+        down = (cell[0]+1, cell[1])
+        left = (cell[0], cell[1]-1)
+        right = (cell[0], cell[1]+1)
+        ul = (cell[0]-1, cell[1]-1)
+        ur = (cell[0]-1, cell[1]+1)
+        dl = (cell[0]+1, cell[1]-1)
+        dr = (cell[0]+1, cell[1]+1)
+        udlr = {"up": up, "down": down, "left": left,"right": right,
+                'ul': ul, 'ur': ur, 'dl': dl, 'dr': dr}
+        return udlr 
+
+def calc_total_price(raw_map, pricing):
+    parsed_map = parse_map(raw_map)
+    total_price = 0
+    plotted = set()
+    for row in range(len(parsed_map)):
+        for col in range(len(parsed_map[0])):
+            plot = (row, col)
+            if plot not in plotted:
+                region = Region(parsed_map, plot)
+                if pricing == "standard":
+                    total_price += region.price()
+                else:
+                    total_price += region.discounted_price()
+                plotted = plotted.union(region.plots)
+    return total_price
+
+def part1(raw_map):
+    return calc_total_price(raw_map, "standard")
+
+    parsed_map = parse_map(raw_map)
+    total_price = 0
+    plotted = set()
+    for row in range(len(parsed_map)):
+        for col in range(len(parsed_map[0])):
+            plot = (row, col)
+            if plot not in plotted:
+                region = Region(parsed_map, plot)
+                total_price += region.price()
+                plotted = plotted.union(region.plots)
+    return total_price
+
+
+def part2(raw_map):
+    return calc_total_price(raw_map, "discounted")
+    parsed_map = parse_map(raw_map)
+    total_price = 0
+    plotted = set()
+    for row in range(len(parsed_map)):
+        for col in range(len(parsed_map[0])):
+            plot = (row, col)
+            if plot not in plotted:
+                region = Region(parsed_map, plot)
+                total_price += region.discounted_price()
+                plotted = plotted.union(region.plots)
+    return total_price
---
+++ 2024/day12/test_code12_refactor.py
@@ -0,0 +1,114 @@
+import pytest
+from code12 import parse_map, Region, part1, part2
+from data12 import *
+
+
+def test_parse_map():
+    parsed_map = parse_map(map1)
+    print(parsed_map)
+    assert len(parsed_map) == 4
+    assert len(parsed_map[1]) == 4
+    assert isinstance(parsed_map, list)
+    assert isinstance(parsed_map[1], list)
+
+parsed_map1 = parse_map(map1)
+
+def assert_raw_map(raw_map, starting, area, perimeter, sides = None):
+    parsed_map = parse_map(raw_map)
+    region = Region(parsed_map, starting)
+    assert region.area == area
+    assert region.perimeter == perimeter
+    assert region.price() == area * perimeter
+    if sides is not None:
+        assert region.sides == sides
+
+def test_region_get_value():
+    """it returns the value"""
+    region = Region(parsed_map1, (0,0))
+    assert region.get_value(*(0,0)) == "A"
+    assert region.get_value(*(3,3)) == "C"
+    """it return None for out of bounds"""
+    assert region.get_value(*(99,99)) == None
+    assert region.get_value(*(4,4)) == None
+
+def test_region_plots():
+    """it returns the coordinates of the plot cells"""
+    region = Region(parsed_map1, (0,0))
+    plots = region.plots
+    assert isinstance(plots, set)
+    assert len(plots) == 4
+    assert (0,3) in plots
+    assert (1,1) not in plots
+
+def test_map_0():
+    """it find the price of a single plot"""
+    assert_raw_map("0", (0,0), 1, 4, 4)
+
+def test_map_00():
+    """it finds the price of a connected plot"""
+    assert_raw_map("00", (0,0), 2, 6, 4)
+
+def test_map_0000():
+    """it finds the price of a plot on 2 rows"""
+    assert_raw_map("00\n00", (0,0), 4, 8, 4)
+
+def test_map_0011():
+    """it finds the price of a garden with multiple plots"""
+    assert_raw_map("00\n11", (0,0), 2, 6)
+    assert_raw_map("00\n11", (1,1), 2, 6)
+
+def test_map1_A():
+    """it finds the A of the first garden map"""
+    assert_raw_map(map1, (0,0), 4, 10)
+
+def test_map1_b():
+    """it finds the B of the first garden map"""
+    assert_raw_map(map1, (1,0), 4, 8)
+
+def test_map1_c():
+    """it finds the C of the first garden map"""
+    assert_raw_map(map1, (2,2), 4, 10)
+
+def test_map1_E():
+    """it finds the E of the first garden map"""
+    assert_raw_map(map1, (3,2), 3, 8)
+
+def test_region_plot_corners():
+    """it counts the correct sides for simple maps"""
+    parsed_map = parse_map("0")
+    cell = (0,0)
+    region = Region(parsed_map, cell)
+    assert region.sides == 4
+    region = Region(parse_map("00"), cell)
+    assert region.sides == 4
+    region = Region(parse_map("000"), cell)
+    assert region.sides == 4
+
+def assert_region_sides(map, cell, sides):
+    parsed_map = parse_map(map)
+    region = Region(parsed_map, cell)
+    assert region.sides == sides
+
+def test_region_sides_outside():
+    """it counts outside corners"""
+    assert_region_sides(map1, (0,0), 4)
+    assert_region_sides(map1, (1,0), 4)
+    assert_region_sides(map1, (3,2), 4)
+
+def test_region_sides_inside():
+    """it counts inside corners"""
+    assert_region_sides(map1, (2,2), 8)
+
+def test_part1():
+    """it returns the total price of the garden"""
+    assert part1(map1) == 140
+    assert part1(map2) == 772
+    assert part1(map3) == 1930
+    assert part1(puzzle)== 1485656
+
+def test_part2():
+    """it returns the discounted price of the garden"""
+    assert part2(map1) == 80
+    assert part2(map_e) == 236
+    assert part2(map_ab) == 368
+    assert part2(puzzle) == 899196
