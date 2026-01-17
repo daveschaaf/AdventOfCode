67a381d496cb2b387964e701864cbf7064b58439
David Schaaf <davidschaaf@berkeley.edu>
2026-01-16 21:20:36 -0500

    Part 1 passed, have to implement large integer linear algebra

--- 2024/day13/code13.py
+++ 2024/day13/code13.py
@@ -1,3 +1,7 @@
+import numpy as np
+from decimal import Decimal
+import sympy as sp
+
 def parse_data(raw_data):
     machine_data = raw_data.split("\n\n")
     parsed_data = []
@@ -9,11 +13,49 @@ def parse_data(raw_data):
         parsed_machine = {}
         parsed_machine["A"] = [ int(move.split("+")[1]) for move in a_button]
         parsed_machine["B"] = [ int(move.split("+")[1]) for move in b_button]
-        parsed_machine["prize"] = [ int(move.split("=")[1]) for move in prize]
+        parsed_machine["prize"] = tuple([ int(move.split("=")[1]) for move in prize])
         parsed_data.append(parsed_machine)
     return parsed_data
 
 class ClawMachine():
     def __init__(self, machine_data):
-       self.machine_data = machine_data
+        self.a_increase_x = machine_data["A"][0]
+        self.a_increase_y = machine_data["A"][1]
+        self.b_increase_x = machine_data["B"][0]
+        self.b_increase_y = machine_data["B"][1]
+        self.a_cost = 3
+        self.b_cost = 1
+        self.matrix = np.array([[self.a_increase_x, self.b_increase_x],
+                                [self.a_increase_y, self.b_increase_y]])
+        self.prize = np.array(machine_data['prize'])
+        self.converted_prize = np.array([self.prize + Decimal(10000000000000)], dtype=object)
+
+    def solution(self, prize):
+        solution = np.linalg.solve(self.matrix, prize)
+        if np.all(np.isclose(solution, np.round(solution), atol=1e-15)):
+            return solution
+        return np.array([0, 0])
+
+    def sympy_solution(self):
+        pass
+        # TODO implement self.solution with sympy
+
+    def solve(self):
+        return np.dot(self.solution(self.prize), np.array([3, 1]))
+
+    def big_solve(self):
+        solution = self.solution(self.converted_prize)
+        print(f"{solution=}")
+        print(f"{np.dot(solution, np.array([3, 1]))}")
+        return np.dot(solution, np.array([3, 1]))
+
+
+def part1():
+    with open('data13.dat', 'r') as file:
+        file_content = file.read()
+    data = parse_data(file_content)
+    tokens = 0
+    for machine in data:
+        tokens += ClawMachine(machine).solve()
+    return tokens
 
--- 2024/day13/test_code13.py
+++ 2024/day13/test_code13.py
@@ -1,4 +1,7 @@
-from code13 import parse_data, ClawMachine
+import numpy as np
+import numpy.testing as npt
+from code13 import parse_data, ClawMachine, part1
+
 sample = """Button A: X+94, Y+34
 Button B: X+22, Y+67
 Prize: X=8400, Y=5400
@@ -21,5 +24,57 @@ def test_parse_data():
     machine_data = data[0]
     assert machine_data["A"] == [94, 34]
     assert machine_data["B"] == [22, 67]
-    assert machine_data["prize"] == [8400, 5400]
+    assert machine_data["prize"] == (8400, 5400)
 
+def test_claw_machine():
+    data = parse_data(sample)
+    claw = ClawMachine(data[0])
+    npt.assert_allclose(claw.prize, np.array((8400, 5400)))
+    assert claw.a_increase_x == 94
+    assert claw.a_increase_y == 34
+    assert claw.b_increase_x == 22
+    assert claw.b_increase_y == 67
+
+def test_claw_solution():
+    data = parse_data(sample)
+    claw1 = ClawMachine(data[0])
+    claw2 = ClawMachine(data[1])
+    claw3 = ClawMachine(data[2])
+    claw4 = ClawMachine(data[3])
+
+    npt.assert_allclose(claw1.solution(claw1.prize), np.array([80, 40]))
+    npt.assert_allclose(claw2.solution(claw2.prize), np.array([0, 0]))
+    npt.assert_allclose(claw3.solution(claw3.prize), np.array([38, 86]))
+    npt.assert_allclose(claw4.solution(claw4.prize), np.array([0, 0]))
+
+def test_claw_solve():
+    data = parse_data(sample)
+    claw1 = ClawMachine(data[0])
+    claw2 = ClawMachine(data[1])
+    claw3 = ClawMachine(data[2])
+    claw4 = ClawMachine(data[3])
+    assert claw1.solve() == 280
+    assert claw2.solve() == 0
+    assert claw3.solve() == 200
+    assert claw4.solve() == 0
+
+def test_part_1():
+    assert part1() == 35255
+    # 26767 too low
+
+def test_converted_prize():
+    data = parse_data(sample)
+    claw4 = ClawMachine(data[3])
+    npt.assert_allclose(claw4.converted_prize, claw4.prize + 10000000000000)
+
+def test_big_solve():
+    data = parse_data(sample)
+    claw1 = ClawMachine(data[0])
+    claw2 = ClawMachine(data[1])
+    claw3 = ClawMachine(data[2])
+    claw4 = ClawMachine(data[3])
+    
+    assert claw1.big_solve() == 0
+    assert claw2.big_solve() > 0
+    assert claw3.big_solve() == 0
+    assert claw4.big_solve() > 0
